add03e8e5850bb9b087d68d12f5052251c0b3cb5
Adam Ondrejka <adulir@atlas.cz>
2025-10-08 12:04:29 +0200

    driver for shift register
    Signed-off-by: Adam Ondrejka <adulir@atlas.cz>

---
+++ MKS3/Core/Inc/sct.h
@@ -0,0 +1,18 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 8, 2025
+ *      Author: xondre13
+ */
+
+#ifndef INC_SCT_H_
+#define INC_SCT_H_
+
+#include "main.h"
+
+void sct_led( uint32_t value );
+void sct_init();
+
+
+
+#endif /* INC_SCT_H_ */
