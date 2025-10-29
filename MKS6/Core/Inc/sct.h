70ced02dbd49c85b03189a400f1aa73b03b65a5c
Adam Ondrejka <adulir@atlas.cz>
2025-10-29 12:20:16 +0100

    DS18B20 temperature sensor and 1-wire bus
    Signed-off-by: Adam Ondrejka <adulir@atlas.cz>

---
+++ MKS6/Core/Inc/sct.h
@@ -0,0 +1,19 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 8, 2025
+ *      Author: xondre13
+ */
+
+#ifndef INC_SCT_H_
+#define INC_SCT_H_
+
+#include "main.h"
+
+void sct_led( uint32_t value );
+void sct_init();
+void sct_value( uint16_t value, uint8_t led, uint8_t pointIndex);
+
+
+
+#endif /* INC_SCT_H_ */
